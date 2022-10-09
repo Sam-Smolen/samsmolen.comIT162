abcf1b269a13ae1016f6badbd93a7839a6bb8d22
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2022-10-09 16:06:00 -0700

    updated files

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>IT162 Portal - Sam Smolen</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/forms.css" />
+</head>
+
+<body>
+    <main class="wrapper">
+        <header>
+          <h1><a href="index.php"><i class="logo fa fa-home"></i> Sam Smolen's SCC IT162 Portal</a></h1>
+          <nav class="topnav" id="myTopnav">
+            <a href="index.php" class="active">Welcome</a>
+            <a href="big/index.php">BIG</a>
+            <a href="aia.php">AIA</a>
+            <a href="flowchart.php">Flowchart</a>
+            <a href="fp/index.php">Final Project</a>
+            <a href="contactme.php">Contact Sam</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+          </nav>
+        </header>
+        <div class="wrapper">
+          <!--header ends here-->
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php';?>
+            <img class="desktop" src="images/golfing.jpeg" alt="golfing" />
+              
+            <img class="tablet" src="images/desktop.jpg" alt="me" />
+              
+            <img class="phone" src="images/tablet.jpg" alt="my dog" />
+              
+              <h2 class="subheader">A little about me!</h2>
+              
+              <p>My name is Sam Smolen. I'm currently studying computer programming at Seattle Central College while also employed as a general contractor. My career interests in programming are in the backend rather than design. Although I want to learn how to build full stack applications.  </p>
+              <p>I like to spend my freetime with my girlfriend, family or friends, and also my dog Moose.</p>
+              <p>Hobbies of mine would be playing video games, golfing, playing frisbee or working out. </p>
+        <?php include 'includes/footer.php';?>
